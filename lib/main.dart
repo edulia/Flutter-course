530a92903931c2d16a95b98e8a03497f06fce050
ArmanAnsari <armanansari04.edugaon@gmail.com>
2022-04-14 07:12:53 +0530

    work on Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
 
-import 'form_screen.dart';
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "First App",
+    home: HomePage(),
+  ));
+}
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'coding with ansar',
-      theme:  ThemeData(
-        primarySwatch: Colors.blueGrey
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
       ),
-      home: FormScreen(),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          width: 100,
+          alignment: Alignment.center,
+          height: 100,
+          decoration: const BoxDecoration(
+              color: Colors.teal,
+
+              // shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 5,
+                  offset: Offset(2.0, 10.0)
+                )
+              ],
+              gradient: LinearGradient(
+                  colors: [Colors.yellow, Colors.pink])),
+          child: const Text(
+            "hello world",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
